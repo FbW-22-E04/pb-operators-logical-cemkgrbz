dec26a0c35d2aa170009f24e8fbacea98d494a6e
Cem <cemkgrbz@gmail.com>
2022-07-06 14:54:46 +0200

    modified js file

---
+++ solution.js
@@ -0,0 +1,27 @@
+//1
+
+const isDogBetter = 'true';
+const isCatBetter = 'false';
+
+//a 
+
+console.log(isDogBetter && isCatBetter);
+console.log(isDogBetter || isCatBetter);
+console.log(!(isDogBetter && isCatBetter));
+
+//2
+
+const atoms = 530;
+const starsInSky = 350;
+const sandGrains = 865;
+
+//3
+
+console.log(atoms > starsInSky && atoms > sandGrains);
+console.log(atoms !== sandGrains);
+console.log(starsInSky < sandGrains || starsInSky > atoms)
+console.log(atoms === starsInSky || atoms !== sandGrains)
+console.log(atoms >= 10 && sandGrains <= 10 )
+console.log(atoms * starsInSky < 100 || atoms * sandGrains > 100)
+
+//4 
