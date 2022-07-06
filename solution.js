ba0422216c1fd52bfaaa487e0084c4d4efab9a48
Cem <cemkgrbz@gmail.com>
2022-07-06 15:21:40 +0200

    modified js file

--- solution.js
+++ solution.js
@@ -18,8 +18,8 @@ const sandGrains = 865;
 //3
 
 console.log(atoms > starsInSky && atoms > sandGrains);
-console.log(atoms != sandGrains);
+console.log(atoms !== sandGrains);
 console.log(starsInSky < sandGrains || starsInSky > atoms)
-console.log(atoms == starsInSky || atoms != sandGrains)
+console.log(atoms == starsInSky || atoms !== sandGrains)
 console.log(atoms >= 10 && sandGrains <= 10 )
 console.log(atoms * starsInSky < 100 || atoms * sandGrains > 100)
